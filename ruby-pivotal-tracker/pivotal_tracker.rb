15377fbe02822e7f94c255e6ef508ef2afbf8c8d
Benjamin Stein <ben@mcommons.com>
2008-12-14 10:46:17 -0500

    add support for long emails and a big update to the tracker ruby client

--- ruby-pivotal-tracker/pivotal_tracker.rb
+++ ruby-pivotal-tracker/pivotal_tracker.rb
@@ -6,14 +6,11 @@ require 'cgi'
 
 ##
 # Pivotal Tracker API Ruby Wrapper
-# November 11, 2008
-# Justin Smestad
-# http://www.evalcode.com
 ##
 
 class Tracker
   
-  def initialize(project_id = '2893', token = '25a6a078f67d9210d2fba91f8c484e7b', ssl=true)
+  def initialize(project_id = '123', token = '45a6a078f67d9210d2fba91f8c484e7b', ssl=true)
     @project_id, @token, @ssl = project_id, token, ssl
     protocol = @ssl ? 'https' : 'http'
     port     = @ssl ? '443'   : '80'
@@ -28,9 +25,7 @@ class Tracker
     end
     validate_response(response.body)
     doc = Hpricot(response.body).at('project')
-
-    @project = {
-      :name             => doc.at('name').innerHTML,
+    { :name             => doc.at('name').innerHTML,
       :iteration_length => doc.at('iteration_length').innerHTML,
       :week_start_day   => doc.at('week_start_day').innerHTML,
       :point_scale      => doc.at('point_scale').innerHTML
@@ -94,15 +89,7 @@ class Tracker
       http.get(resource_uri.path, {'Token' => @token, 'Content-Type' => 'application/xml'})
     end
     validate_response(response.body)
-    
-    doc = Hpricot(response.body).at('story')
-    
-    @story = {
-      :id   => doc.at('id').innerHTML.to_i,
-      :type => doc.at('story_type').innerHTML,
-      :name => doc.at('name').innerHTML,
-      :url  => doc.at('url').innerHTML
-    }
+    story_xml_to_hash(response.body)
   end
   
   def create_story(story)
@@ -112,12 +99,7 @@ class Tracker
       http.post(resource_uri.path, story_xml, {'Token' => @token, 'Content-Type' => 'application/xml'})
     end
     validate_response(response.body)
-    doc = Hpricot(response.body).at('story')
-    { :id   => doc.at('id').innerHTML.to_i,
-      :type => doc.at('story_type').innerHTML,
-      :name => doc.at('name').innerHTML,
-      :url  => doc.at('url').innerHTML
-    }
+    story_xml_to_hash(response.body)
   end
   
   def update_story(story)
@@ -127,8 +109,7 @@ class Tracker
       http.put(resource_uri.path, story_xml, {'Token' => @token, 'Content-Type' => 'application/xml'})
     end
     validate_response(response.body)
-    
-    story[:id]
+    story_xml_to_hash(response.body)
   end
   
   def delete_story(story_id)
@@ -140,32 +121,60 @@ class Tracker
     story_id
   end
   
-  private
+  def add_comment(story_id, text)
+    resource_uri = URI.parse("#{@base_url}/#{@project_id}/stories/#{story_id}/notes")
+    comment_xml = "<note><text>#{text}</text></note>"
+    response = net_http(resource_uri).start do |http|
+      http.post(resource_uri.path, comment_xml, {'Token' => @token, 'Content-Type' => 'application/xml'})
+    end
+    validate_response(response.body)
+    doc = Hpricot(response.body).at('note')
+    { :id     => doc.at('id').innerHTML.to_i,
+      :text   => doc.at('text').innerHTML,
+      :author => doc.at('author').innerHTML,
+      :date   => doc.at('date').innerHTML    }
+  end
+  
+private
     
-    def build_story_xml(story)
-      story_xml = "<story>"
-      story.each do |key, value|
-        story_xml << "<#{key}>#{remove_xml_tags(value.to_s)}</#{key}>"
-      end
-      story_xml << "</story>"
+  def build_story_xml(story)
+    story_xml = "<story>"
+    story.each do |key, value|
+      story_xml << "<#{key}>#{remove_xml_tags(value.to_s)}</#{key}>"
     end
+    story_xml << "</story>"
+  end
 
-    def validate_response(body)
-      response = Hpricot(body).at('response')
-      if response[:success]=='false'      
-        raise TrackerException.new((response/'message').innerHTML)
-      end
+  def validate_response(body)
+    response = Hpricot(body).at('response')
+    if response[:success]=='false'      
+      raise TrackerException.new((response/'message').innerHTML)
     end
+  end
 
-    def remove_xml_tags(xml)
-      xml.gsub(/<\/?[^>]*>/, "")
-    end
+  def remove_xml_tags(xml)
+    xml.gsub(/<\/?[^>]*>/, "")
+  end
 
-    def net_http(uri)
-      h = Net::HTTP.new(uri.host, uri.port)
-      h.use_ssl = @ssl
-      h
-    end
+  def net_http(uri)
+    h = Net::HTTP.new(uri.host, uri.port)
+    h.use_ssl = @ssl
+    h
+  end
+
+  def story_xml_to_hash(xml)
+    doc = Hpricot(xml).at('story')
+    { :id            => doc.at('id').innerHTML.to_i,
+      :story_type    => doc.at('story_type').innerHTML,
+      :name          => doc.at('name').innerHTML,
+      :estimate      => doc.at('estimate').innerHTML,
+      :current_state => doc.at('current_state').innerHTML,
+      :description   => doc.at('description').innerHTML,
+      :url           => doc.at('url').innerHTML
+      :requested_by  => doc.at('requested_by').innerHTML.to_i,
+      :created_at    => doc.at('created_at').innerHTML.to_i
+    }      
+  end
 
 end
 
